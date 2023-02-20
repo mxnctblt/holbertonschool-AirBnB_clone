96891208abd355dfe92c6145c92c242a8ad58ec5
AntoineJacob <5646@holbertonstudents.com>
2023-02-20 19:33:19 +0100

    please can u work one time ?

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
                 if arg != '__class__':
                     setattr(self, arg, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
             models.storage.new(self)
