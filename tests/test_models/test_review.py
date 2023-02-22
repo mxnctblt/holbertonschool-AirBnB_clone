f9580dcfa878ae7f3a02ca213ac1a8cdefe9123b
AntoineJacob <5646@holbertonstudents.com>
2023-02-22 16:40:34 +0100

    test review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,36 +1,56 @@
 #!/usr/bin/python3
-"""Test Review"""
+"""
+Unitesting for review
+"""
 import unittest
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
+import os
 from models.review import Review
-from models.state import State
+from models.base_model import BaseModel
 
 
-class Testreview(unittest.TestCase):
-    """
-    Unittests for the Review class.
-    """
+class TestReview(unittest.TestCase):
 
-    def test_class(self):
-        """
-        Tests to check if class is named correctly.
-        """
-        rev1 = Review()
-        self.assertEqual(rev1.__class__.__name__, "Review")
+    @classmethod
+    def setUpClass(cls):
+        cls.Test1 = Review()
+        cls.Test1.place_id = "Test"
+        cls.Test1.user_id = "Test2"
+        cls.Test1.text = "Test3"
 
-    def test_father(self):
-        """
-        Tests to check if lass inherits from BaseModel.
-        """
-        rev1 = Review()
-        self.assertTrue(issubclass(rev1.__class__, BaseModel))
+    @classmethod
+    def tearDownClass(cls):
+        del cls.Test1
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
 
-    def test_none(self):
-        bm1 = BaseModel(None)
-        self.assertNotIn(None, bm1.__dict__)
+    def test_is_sub(self):
+        self.assertTrue(issubclass(self.Test1.__class__, BaseModel), True)
+
+    def test_checking_for_functions(self):
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_has_attr(self):
+        self.assertTrue('id' in self.Test1.__dict__)
+        self.assertTrue('created_at' in self.Test1.__dict__)
+        self.assertTrue('updated_at' in self.Test1.__dict__)
+        self.assertTrue('place_id' in self.Test1.__dict__)
+        self.assertTrue('text' in self.Test1.__dict__)
+        self.assertTrue('user_id' in self.Test1.__dict__)
+
+    def test_strings(self):
+        self.assertEqual(type(self.Test1.text), str)
+        self.assertEqual(type(self.Test1.place_id), str)
+        self.assertEqual(type(self.Test1.user_id), str)
+
+    def test_save(self):
+        self.Mitali.save()
+        self.assertNotEqual(self.Test1.created_at, self.Test1.updated_at)
+
+    def test_to_dict(self):
+        self.assertEqual('to_dict' in dir(self.Test1), True)
+
 
 if __name__ == "__main__":
     unittest.main()
