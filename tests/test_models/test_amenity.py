d1e6adc8a5f9037c4666551d8f7c2a1e2f92ad6e
AntoineJacob <5646@holbertonstudents.com>
2023-02-23 13:45:48 +0100

    fixing test amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,49 +1,81 @@
 #!/usr/bin/python3
-"""
-module testing amenity
-"""
-import unittest
+""" unittests for User """
 import os
+import models
+import unittest
+from datetime import datetime
 from models.amenity import Amenity
 from models.base_model import BaseModel
+from time import sleep
 
 
 class TestAmenity(unittest.TestCase):
+    """ testing amenity """
 
     @classmethod
     def setUpClass(cls):
+        """set up for test"""
         cls.amen = Amenity()
-        cls.amen.name = "jacuzzi"
+        cls.amen.name = "Antoine"
 
     @classmethod
-    def tearDownClass(cls):
+    def teardown(cls):
+        """ at the end of the test cls will tear it down """
         del cls.amen
+
+    def tearDown(self):
+        """ teardown file.json """
         try:
             os.remove("file.json")
-        except FileNotFoundError:
+        except Exception:
             pass
 
-    def test_subclass(self):
-        self.assertTrue(issubclass(self.amen.__class__, BaseModel), True)
+    def test_types(self):
+        """ testing the types """
+        self.assertEqual(Amenity, type(Amenity()))
+        self.assertEqual(datetime, type(Amenity().created_at))
+        self.assertEqual(datetime, type(Amenity().updated_at))
+        self.assertEqual(str, type(Amenity().id))
+        self.assertEqual(str, type(Amenity.name))
 
-    def test_functions(self):
-        self.assertIsNotNone(Amenity.__doc__)
 
-    def test_attr(self):
+    def test_state(self):
+        """ testing its attributes """
+        self.assertTrue('name' in self.amen.__dict__)
         self.assertTrue('id' in self.amen.__dict__)
         self.assertTrue('created_at' in self.amen.__dict__)
         self.assertTrue('updated_at' in self.amen.__dict__)
-        self.assertTrue('name' in self.amen.__dict__)
-
-    def test_strings(self):
-        self.assertEqual(type(self.amen.name), str)
-
-    def test_save(self):
+        self.assertIn(Amenity(), models.storage.all().values())
+        self.assertEqual(self.amen.name, 'Antoine')
+        self.assertIsNotNone(Amenity.__doc__)
+        self.assertTrue(issubclass(self.amen.__class__, BaseModel), True)
         self.amen.save()
         self.assertNotEqual(self.amen.created_at, self.amen.updated_at)
-
-    def test_to_dict(self):
         self.assertEqual('to_dict' in dir(self.amen), True)
+
+    def test_str(self):
+        """ testing string representation """
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        st = Amenity()
+        st.id = "123456"
+        st.created_at = st.updated_at = dt
+        ststr = st.__str__()
+        self.assertIn("[Amenity] (123456)", ststr)
+        self.assertIn("'id': '123456'", ststr)
+        self.assertIn("'created_at': " + dt_repr, ststr)
+        self.assertIn("'updated_at': " + dt_repr, ststr)
+
+    def test_kwargs(self):
+        """ testing instantation with kwargs """
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        st = Amenity(id="123456", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(st.id, "123456")
+        self.assertEqual(st.created_at, dt)
+        self.assertEqual(st.updated_at, dt)
+        with self.assertRaises(TypeError):
+            Amenity(id=None, created_at=None, updated_at=None)
 
 
 if __name__ == "__main__":
